9dd1dbeba37598994271100ce2136c745179cd06
AndresDZ2024 <daza45dazagmail.com>
2024-04-27 20:35:01 -0500

    feat: :sparkles: Se desarrolló por completo la pagina que recorre la API de pokémon
    Se desarrolló la actividad de la API de pokemon satisfactoriamente

--- Dia 11/js/script.js
+++ Dia 11/js/script.js
@@ -1,12 +1,73 @@
+let searchPokemon = 1;
 
+function fetchpokemon() {
+  let pokemonInput = document.getElementById('Id').value.toLowerCase();
+  let url = `https://pokeapi.co/api/v2/pokemon/${pokemonInput}`;
 
-let buscarPokemon = 1;
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      displaypokemon(data);
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+}
 
-const obtenerPokemon = async (pokemon) => {
-  const respuestaAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+function displaypokemon(data) {
+  let pokemonInfo = document.getElementById("pokeInfo");
 
-  if (respuestaAPI.status === 200) {
-    const datos = await respuestaAPI.json();
-    return datos;
+  if (data.name) {
+    let pokemonImage = data.id >= 650 ? './storage/img/noimg.png' : data.sprites.versions['generation-v']['black-white'].animated.front_default;
+    pokemonInfo.innerHTML = `
+    <div class="pokemon_datos">
+      <span class="pokemon_numero">${data.id} -</span>
+      <span class="pokemon_nombre">${data.name}</span>
+    </div>
+    <img class="pokemon__imagen" src=${pokemonImage}></img>
+    `;
+    searchPokemon = data.id;
+    console.log(searchPokemon)
+  } else {
+    pokemonInfo.innerHTML = `<p>Error: Pokémon not found</p>`;
   }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var input = document.getElementById('Id');
+  input.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13) {
+          fetchpokemon();
+          event.preventDefault();
+      }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttonPrev = document.querySelector('.btn-prev');
+  const buttonNext = document.querySelector('.btn-next');
+
+  buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+      searchPokemon -= 1;
+      renderPokemon(searchPokemon);
+    }
+  });
+
+  buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+  });
+
+  renderPokemon(searchPokemon);
+});
+
+function renderPokemon(searchPokemon) {
+  document.getElementById('Id').value = searchPokemon;
+  fetchpokemon();
 }
